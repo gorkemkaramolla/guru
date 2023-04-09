a2c8298d1a5f0a086c65ed7c8fca279d5c4176e4
Fatih Yavuz <yvzfth@yandex.com>
2023-04-09 13:42:30 +0300

    next auth adapter removed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,72 +1,72 @@
-// // import { Role } from '@/nextauth';
-// import NextAuth, { Account, Profile, User } from 'next-auth';
-// import { Adapter, AdapterUser } from 'next-auth/adapters';
+// import { Role } from '@/nextauth';
+import NextAuth, { Account, Profile, User } from 'next-auth';
+import { Adapter, AdapterUser } from 'next-auth/adapters';
 
-// import { JWT } from 'next-auth/jwt';
-// import GoogleProvider from 'next-auth/providers/google';
+import { JWT } from 'next-auth/jwt';
+import GoogleProvider from 'next-auth/providers/google';
 // import UserModel from '@/model/user.model';
 // import { ObjectId } from 'mongodb';
 // import { NextApiRequest, NextApiResponse } from 'next';
 // import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   adapter: MongoDBAdapter(clientPromise),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
+export const authOptions = {
+  // Configure one or more authentication providers
+  //   adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 
-//   callbacks: {
-//     async jwt({
-//       token,
-//       user,
-//       account,
-//     }: {
-//       token: JWT;
-//       user?: User | AdapterUser | undefined;
-//       account?: Account | null | undefined;
-//       profile?: Profile | undefined;
-//       isNewUser?: boolean | undefined;
-//     }) {
-//       // Persist the OAuth access_token to the token right after signin
-//       if (account) {
-//         // if (user) {
-//         //   if (process.env.ADMINS?.split(',').includes(user?.email!))
-//         //     user.role = 'admin' as Role;
-//         //   token.role = user.role;
-//         // }
+  callbacks: {
+    async jwt({
+      token,
+      user,
+      account,
+    }: {
+      token: JWT;
+      user?: User | AdapterUser | undefined;
+      account?: Account | null | undefined;
+      profile?: Profile | undefined;
+      isNewUser?: boolean | undefined;
+    }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        // if (user) {
+        //   if (process.env.ADMINS?.split(',').includes(user?.email!))
+        //     user.role = 'admin' as Role;
+        //   token.role = user.role;
+        // }
 
-//         token.accessToken = account.access_token;
-//       }
-//       return token;
-//     },
-//     async redirect({ url, baseUrl }: { url: any; baseUrl: any }) {
-//       return baseUrl;
-//     },
-//     async session({ session, token }: { session: any; token: any }) {
-//       // Send properties to the client, like an access_token from a provider.
-//       if (token && session.user) {
-//         // session.user.role = token.role;
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async redirect({ url, baseUrl }: { url: any; baseUrl: any }) {
+      return baseUrl;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      // Send properties to the client, like an access_token from a provider.
+      if (token && session.user) {
+        // session.user.role = token.role;
 
-//         // const newUser = await new userModel({
-//         //   _id: new ObjectId(),
-//         //   email: session.user?.email,
-//         //   name: session.user?.name,
-//         //   image: session.user?.image,
-//         // });
-//         // await newUser.save();
-//         // console.log('user: ' + user);
+        // const newUser = await new userModel({
+        //   _id: new ObjectId(),
+        //   email: session.user?.email,
+        //   name: session.user?.name,
+        //   image: session.user?.image,
+        // });
+        // await newUser.save();
+        // console.log('user: ' + user);
 
-//         session.accessToken = token.accessToken;
-//       }
-//       return session;
-//     },
-//   },
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
 
-//   secret: process.env.NEXTAUTH_SECRET,
-// };
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
-// export default NextAuth(authOptions);
+export default NextAuth(authOptions);
