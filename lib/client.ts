ae7d2f7ce27e102d9739fe08a5b77990ce922c57
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-05-04 20:33:43 +0200

    test

--- lib/client.ts
+++ lib/client.ts
@@ -6,6 +6,7 @@ export const getClient = () => {
   // create a new client if there's no existing one
   // or if we are running on the server.
   if (!client || typeof window === 'undefined') {
+    console.log(process.env.GRAPHQL_HOST);
     client = new ApolloClient({
       link: new HttpLink({
         uri: `${process.env.GRAPHQL_HOST}`,
