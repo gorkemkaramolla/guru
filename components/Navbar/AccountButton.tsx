d44c4e65adc92eba0fcf1448bfa3ff4f0f0767f7
Fatih Yavuz <yvzfth@yandex.com>
2023-05-05 17:28:10 +0300

    setting href fix

--- components/Navbar/AccountButton.tsx
+++ components/Navbar/AccountButton.tsx
@@ -1,6 +1,5 @@
 import { Dropdown, Avatar, Text, User } from '@nextui-org/react';
 import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 export default function App() {
   const session = useSession();
@@ -57,7 +56,7 @@ export default function App() {
         <Dropdown.Item key='settings' withDivider>
           <Text
             onClick={() => {
-              router.push('settings');
+              router.push('/settings');
             }}
           >
             My Settings
@@ -72,49 +71,5 @@ export default function App() {
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
-
-    // <Dropdown placement='bottom-left'>
-    //   <Dropdown.Trigger>
-    //     <User
-    //       bordered
-    //       as='button'
-    //       size='lg'
-    //       color='primary'
-    //       name={session.data?.user?.name}
-    //       description={
-    //         '@' +
-    //         session.data?.user?.name
-    //           ?.replaceAll(' ', '')
-    //           .toLocaleLowerCase()
-    //       }
-    //       src={session.data?.user?.image!}
-    //     />
-    //   </Dropdown.Trigger>
-    //   <Dropdown.Menu color='primary' aria-label='User Actions'>
-    //     <Dropdown.Item key='profile' css={{ height: '$18' }}>
-    //       <Text b color='inherit' css={{ d: 'flex' }}>
-    //         Signed in as
-    //       </Text>
-    //       <Text b color='inherit' css={{ d: 'flex' }}>
-    //         {session.data?.user?.email}
-    //       </Text>
-    //     </Dropdown.Item>
-    //     <Dropdown.Item key='settings' withDivider>
-    //       My Settings
-    //     </Dropdown.Item>
-    //     <Dropdown.Item key='team_settings'>Team Settings</Dropdown.Item>
-    //     <Dropdown.Item key='analytics' withDivider>
-    //       Analytics
-    //     </Dropdown.Item>
-    //     <Dropdown.Item key='system'>System</Dropdown.Item>
-    //     <Dropdown.Item key='configurations'>Configurations</Dropdown.Item>
-    //     <Dropdown.Item key='help_and_feedback' withDivider>
-    //       Help & Feedback
-    //     </Dropdown.Item>
-    //     <Dropdown.Item key='logout' color='error' withDivider  >
-    //       Log Out
-    //     </Dropdown.Item>
-    //   </Dropdown.Menu>
-    // </Dropdown>
   );
 }
